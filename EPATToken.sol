98ee748ceed45e22925590e6a5a09e7309d69aa6
vudoanthang <vudoanthang83@gmail.com>
2022-05-25 18:19:18 +0700

    Update EPATToken.sol

--- EPATToken.sol
+++ EPATToken.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EPATToken is ERC20 {
+contract EPATToken is ERC20, Ownable {
 	constructor() ERC20("EPATToken", "EPAT") {
 		_mint(msg.sender, 1000000000 * 10 ** decimals());
 	}
