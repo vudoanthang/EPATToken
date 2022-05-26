06d8d4cf6e2ccab48b80f61a070da37667a8223c
vudoanthang <vudoanthang83@gmail.com>
2022-05-26 16:45:26 +0700

    Create devepat-token.sol

---
+++ devepat-token.sol
@@ -0,0 +1,83 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC20 {
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address from, address to, uint256 amount) external returns (bool);
+}
+
+contract DevePatToken is IERC20 {
+    string constant public name = "DevePat Token";
+    string constant public symbol = "EPAT";
+    uint8 constant public decimals = 18;
+    uint256 constant public totalSupply = 1000000000000000000000000000;
+
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+
+    constructor() {
+        _balances[msg.sender] = totalSupply;
+        emit Transfer(address(0), msg.sender, totalSupply);
+    }
+
+
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address to, uint256 amount) public virtual override returns (bool) {
+        _transfer(msg.sender, to, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(msg.sender, spender, amount);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public virtual override returns (bool) {
+        require(_allowances[from][msg.sender] >= amount, "Insufficient allowance");
+        unchecked {
+            _approve(from, msg.sender, _allowances[from][msg.sender] - amount);
+        }
+        _transfer(from, to, amount);
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(msg.sender, spender, allowance(msg.sender, spender) + addedValue);
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        require(_allowances[msg.sender][spender] >= subtractedValue, "Decreased allowance below zero");
+        unchecked {
+            _approve(msg.sender, spender, _allowances[msg.sender][spender] - subtractedValue);
+        }
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) internal virtual {
+        require(_balances[from] >= amount, "Transfer amount exceeds balance");
+        unchecked {
+            _balances[from] = _balances[from] - amount;
+        }
+        _balances[to] += amount;
+        emit Transfer(from, to, amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+}
